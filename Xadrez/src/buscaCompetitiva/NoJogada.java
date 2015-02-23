f7bef8030b43f873f971fa2eac321c6d505e63fa
fabio <ffabio.ns@hotmail.com>
2015-02-23 01:41:22 -0300

    Criação da Classe que o metodo de getAllJogadas ira chamar e ir montando a arvore a cada recursao

---
+++ Xadrez/src/buscaCompetitiva/NoJogada.java
@@ -0,0 +1,113 @@
+package buscaCompetitiva;
+
+import java.util.ArrayList;
+import principal.Tabuleiro;
+
+/**
+ *
+ * @author fabio
+ */
+// Classe que o metodo de getAllJogadas ira chamar e ir montando a arvore
+// a cada recursao
+
+public class NoJogada {
+    
+    private Tabuleiro tabuleiro;
+    private int utilidade;
+    private int alpha;
+    private int beta;
+    private int profundidade;
+    private static int profundidadeLimite;
+    //proximosNoJogadas = todos as configurações dos tabuleiros
+    //para todas jogadas possíveis        
+    private ArrayList<NoJogada> proximosNoJogadas;
+    
+    //O No Pai, que é a jogada anterior
+    private NoJogada noJogadaAnterior ;
+
+    public NoJogada( NoJogada noJogadaAnterior ,Tabuleiro tabuleiro,
+            ArrayList<NoJogada> proximosNoJogadas, 
+            int utilidade, int profundidade, int profundidadeLimite) {
+        this(noJogadaAnterior, tabuleiro ,proximosNoJogadas , utilidade, profundidade);
+        NoJogada.profundidadeLimite = profundidadeLimite;
+    }
+    
+      public NoJogada(NoJogada noJogadaAnterior, Tabuleiro tabuleiro,
+              ArrayList<NoJogada> proximosNoJogadas, 
+              int utilidade, int profundidade) {
+        this.tabuleiro = tabuleiro;
+        this.utilidade = utilidade;
+        this.profundidade = profundidade;
+        this.proximosNoJogadas = proximosNoJogadas;
+    }
+      
+      
+      
+
+    public Tabuleiro getTabuleiro() {
+        return tabuleiro;
+    }
+
+    public void setTabuleiro(Tabuleiro tabuleiro) {
+        this.tabuleiro = tabuleiro;
+    }
+
+    public int getUtilidade() {
+        return utilidade;
+    }
+
+    public void setUtilidade(int utilidade) {
+        this.utilidade = utilidade;
+    }
+
+    public int getAlpha() {
+        return alpha;
+    }
+
+    public void setAlpha(int alpha) {
+        this.alpha = alpha;
+    }
+
+    public int getBeta() {
+        return beta;
+    }
+
+    public void setBeta(int beta) {
+        this.beta = beta;
+    }
+
+    public int getProfundidade() {
+        return profundidade;
+    }
+
+    public void setProfundidade(int profundidade) {
+        this.profundidade = profundidade;
+    }
+
+    public static int getProfundidadeLimite() {
+        return profundidadeLimite;
+    }
+
+    public static void setProfundidadeLimite(int profundidadeLimite) {
+        NoJogada.profundidadeLimite = profundidadeLimite;
+    }
+
+    public ArrayList<NoJogada> getProximosNoJogadas() {
+        return proximosNoJogadas;
+    }
+
+    public void setProximosNoJogadas(ArrayList<NoJogada> proximosNoJogadas) {
+        this.proximosNoJogadas = proximosNoJogadas;
+    }
+
+    public NoJogada getNoJogadaAnterior() {
+        return noJogadaAnterior;
+    }
+
+    public void setNoJogadaAnterior(NoJogada noJogadaAnterior) {
+        this.noJogadaAnterior = noJogadaAnterior;
+    }
+
+      
+      
+}
